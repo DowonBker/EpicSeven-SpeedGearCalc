4c510ed8fb6c04bd9a94412d1926ac71ef3656b9
DowonBker <bker.rhythm@gmail.com>
2025-07-26 03:01:47 +0000

    Refactor: 입력 검증 보완 및 확률 계산 로직 정리

--- src/epic7speedcalc.c
+++ src/epic7speedcalc.c
@@ -4,18 +4,19 @@
 */
 #include <stdio.h>
 #include <math.h>
+#define MAX_SPD 35  //속도 최댓값이 34, spd_perc[34]가 속도 34를 가르키기위해 1을 더한 값을 선언
 
-void enh_proc_lgd();
-void enh_proc_epic();
-void reforge();
-void enhance();
-void enhance_lgd();
-void enhance_epic();
+void enh_proc_lgd(double perc[MAX_SPD]);
+void enh_proc_epic(double perc[MAX_SPD]);
+void reforge(double perc[MAX_SPD], int times);
+void enhance_lgd(double perc[MAX_SPD]);
+void enhance_epic(double perc[MAX_SPD]);
+void copy_array(double dest[MAX_SPD], double src[MAX_SPD]);
 
 //이항분포
-int facto();
-int binomi_coeffi();
-double binomi_probabil();
+int facto(int n);
+int binomi_coeffi(int n, int k);
+double binomi_probabil(int n, int k, double p);
 
 
 int main(void){
@@ -24,19 +25,19 @@ int main(void){
 	
 	int item_class;
 	printf("강화할 아이템이\n전설템이면 1, 영웅템이면 2를 입력해주세요\n");
-	scanf("%d", &item_class);
 	
-	if(item_class!=1 && item_class!=2){
+    if(scanf("%d", &item_class) != 1 || (item_class != 1 && item_class != 2)){
 		printf("잘못된 입력입니다\n");
 		return 0;
 	}
 	
-	double spd_perc[35]={0};	//속도 최댓값이 34
 	
 	int s0;
 	printf("초기 속도값을 입력해주세요\n");
-	scanf("%d", &s0);
-	spd_perc[s0]=1;
+	if (scanf("%d", &s0) != 1){
+        printf("잘못된 입력입니다\n");
+        return 0;
+    }
 	
 	if(item_class==1 && (s0<2 || s0>5)){
 		printf("잘못된 입력입니다\n");
@@ -46,6 +47,10 @@ int main(void){
 		printf("잘못된 입력입니다\n");
 		return 0;
 	}
+
+	double spd_perc[MAX_SPD]={0};
+	spd_perc[s0]=1;
+
     printf("\n\n");
 	
     if(item_class==1)
@@ -57,7 +62,7 @@ int main(void){
 }
 
 
-void enh_proc_lgd(double perc[35]){
+void enh_proc_lgd(double perc[MAX_SPD]){
 	for(int i=25;i>0;i--){
 		if(perc[i]>0){
 			perc[i+2]+=perc[i]*0.33223;
@@ -69,7 +74,7 @@ void enh_proc_lgd(double perc[35]){
 	}
 }
 
-void enh_proc_epic(double perc[35]){
+void enh_proc_epic(double perc[MAX_SPD]){
 	for(int i=16;i>0;i--){
 		if(perc[i]>0){
 			perc[i+1]+=perc[i]*0.03833;
@@ -81,37 +86,31 @@ void enh_proc_epic(double perc[35]){
 	}
 }
 
-void reforge(double perc[35], int times){    //제련
+void reforge(double perc[MAX_SPD], int times){    //제련
 	if(times>4)
         times=4;
-    for(int i=(34-times);i>0;i--){
+    for(int i=(MAX_SPD-1-times);i>0;i--){
         perc[i+times]=perc[i];
         perc[i]=0;
     }
 }
 
-void enh_lgd(int enh_times, double perc[35]){
+void enh_lgd(int enh_times, double perc[MAX_SPD]){
     for(int i=0;i<enh_times;i++)
         enh_proc_lgd(perc);
     reforge(perc, enh_times);
 
-    for(int i=0;i<35;i++)
+    for(int i=0;i<MAX_SPD;i++)
         perc[i]*=binomi_probabil(5,enh_times,0.25);
 }
 
-void enhance_lgd(double perc[35]){
-    double perc2[35];
-    for(int i=0;i<35;i++)
-        perc2[i]=perc[i];
-    double perc3[35];
-    for(int i=0;i<35;i++)
-        perc3[i]=perc[i];
-    double perc4[35];
-    for(int i=0;i<35;i++)
-        perc4[i]=perc[i];
-    double perc5[35];
-    for(int i=0;i<35;i++)
-        perc5[i]=perc[i];
+void enhance_lgd(double perc[MAX_SPD]){
+    double perc2[MAX_SPD], perc3[MAX_SPD], perc4[MAX_SPD], perc5[MAX_SPD];
+    copy_array(perc2, perc);
+    copy_array(perc3, perc);
+    copy_array(perc4, perc);
+    copy_array(perc5, perc);
+
 
     int enh_times=2;
     enh_lgd(enh_times, perc2);
@@ -122,64 +121,66 @@ void enhance_lgd(double perc[35]){
     enh_times++;
     enh_lgd(enh_times, perc5);
 
-    double sum[35];
-    for(int i=0;i<35;i++)
+    double sum[MAX_SPD];
+    for(int i=0;i<MAX_SPD;i++)
         sum[i]=perc2[i]+perc3[i]+perc4[i]+perc5[i];
     
-    for(int i=15;i<35;i++)
+    for(int i=15;i<MAX_SPD;i++)
         if(sum[i]*100>=0.0005)
             printf("속도가 %d 일 확률: %.3lf%%\n", i, sum[i]*100);
     
 }
 
-void enh_epic(int enh_times, double perc[35]){
+void enh_epic(int enh_times, double perc[MAX_SPD]){
     for(int i=0;i<enh_times;i++)
         enh_proc_epic(perc);
     reforge(perc, enh_times);
 
-    for(int i=0;i<35;i++)
+    for(int i=0;i<MAX_SPD;i++)
         perc[i]*=binomi_probabil(5,enh_times,0.25);
 }
 
-void enhance_epic(double perc[35]){
+void enhance_epic(double perc[MAX_SPD]){
 
-    double perc3[35];
-    for(int i=0;i<35;i++)
-        perc3[i]=perc[i];
-    double perc4[35];
-    for(int i=0;i<35;i++)
-        perc4[i]=perc[i];
+    double perc3[MAX_SPD], perc4[MAX_SPD];
+    copy_array(perc3, perc);
+    copy_array(perc4, perc);
 
     for(int i=0;i<3;i++)
         enh_proc_epic(perc3);
     reforge(perc3, 3);
     
-    for(int i=0;i<35;i++)
+    for(int i=0;i<MAX_SPD;i++)
         perc3[i]*=binomi_probabil(3,3,0.333333)*0.75;
 
     for(int i=0;i<4;i++)
         enh_proc_epic(perc4);
     reforge(perc4, 4);
     
-    for(int i=0;i<35;i++)
+    for(int i=0;i<MAX_SPD;i++)
         perc4[i]*=binomi_probabil(3,3,0.333333)*0.25;
 
-    double sum[35];
-    for(int i=0;i<35;i++)
+    double sum[MAX_SPD];
+    for(int i=0;i<MAX_SPD;i++)
         sum[i]=perc3[i]+perc4[i];
 
-    for(int i=15;i<35;i++)
+    for(int i=15;i<MAX_SPD;i++)
         if(sum[i]*100>=0.0005)
             printf("속도가 %d 일 확률: %.3lf%%\n", i, sum[i]*100);
+}
+
+void copy_array(double perc2[MAX_SPD], double perc1[MAX_SPD]){
+    for(int i=0;i<MAX_SPD;i++)
+        perc2[i]=perc1[i];
 }
 
 
 //이항분포
 int facto(int n){
-    int ans=1;
-    for(int i=2;i<=n;i++)
-        ans*=i;
-    return ans;
+    static const int fac[]={
+        1, 1, 2, 6, 24, 120
+    };
+    return fac[n];
 }
 
 int binomi_coeffi(int n, int k){
